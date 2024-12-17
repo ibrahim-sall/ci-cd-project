58230770ce841eda3f8bf6f91572870fdf737f67
ibrahim sall <ibhou2002@gmail.com>
2024-12-17 21:16:12 +0000

    comand line to get list of vehicles with a nice json file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const { program } = require("@caporal/core")
 
 program
-  .command("list-vehicle", "Add a vehicle to the database")
+  .command("list-vehicle", "List all vehicles")
   .option("-p, --port <port>", "Port to use", {
     default: "3000",
   })
   .action(({ logger, options }) => {
-    const url = 'http://localhost:'+options.port+'/create-vehicle';
-    logger.info(url)
-    //Faire un fetch
-  })
+    const url = 'http://localhost:'+options.port+'/vehicles';
+    logger.info(url);
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        logger.info('Vehicles:', data);
+      })
+      .catch(error => {
+        logger.error('Error:', error);
+      });
+  });
 
-// always run the program at the end
+
 program.run()

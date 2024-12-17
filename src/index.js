4b4cdcebff5428147cde2d8526fc1e8efbf360bd
Vincent Miras <vincent.miras03@gmail.com>
2024-12-17 16:21:52 +0000

    creation commande remove

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const { program } = require("@caporal/core")
 
-// Simplest program ever: this program does only one thing
-program.action(({ logger }) => {
-  logger.info("Hello, world!")
-})
+program
+  .command("add", "Add a vehicle to the database")
+  .option("--id <id>","ID of the vehicle you want to remove")
+  .option("-p, --port <port>", "Port to use", {
+    default: "3000",
+  })
+  .action(({ logger, options }) => {
+    const url = 'http://localhost:'+options.port+'/create-vehicle';
+    const data = {
+      id : options.id
+    };
+    logger.info(url)
+    logger.info(data.shortcode)
+    //Faire un fetch
+  })
 
 // always run the program at the end
 program.run()
